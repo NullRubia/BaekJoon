e9325d9a291e0f673db5cf70c64e46edec1324fa
NullRubia <nateriver0527@gmail.com>
2025-03-28 23:53:40 +0900

    Update 2739.py

--- 2025.03.28/2739.py
+++ 2025.03.28/2739.py
@@ -1,21 +1,5 @@
-A, B, C = input().split()
-A = int(A)
-B = int(B)
-C = int(C)
-if A == B and B == C :
-    S = 10000+(A*1000)
-elif A == B or A == C or B == C :
-    if A == B :
-        S = 1000+(A*100)
-    elif A == C :
-        S = 1000+(A*100)
-    else :
-        S = 1000+(B*100)
-else :
-    if A >= B and A >= C :
-        S = A*100
-    elif B >=A and B >= C :
-        S = B*100
-    else :
-        S = C*100
-print(S)
+N = int(input())
+i = 0
+while i < 9 :
+    i = i+1
+    print(N,"*",i,"=",N*i)
