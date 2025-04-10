aa7d8181ad72072bb6ec2e96d5dbfaea0724b70b
NullRubia <nateriver0527@gmail.com>
2025-04-10 23:52:03 +0900

    Create 10172.js

---
+++ level1/10172.js
@@ -0,0 +1,8 @@
+console.log("|\\_/|");
+console.log("|q p|   /}");
+console.log('( 0 )"""\\'); //=console.log("( 0 )\"\"\"\\");
+console.log('|"^"`    |'); //=console.log("|\"^\"`    |");
+console.log("||_/=\\\\__|");
+
+//아래 방법으로도 가능
+//console.log('|\\_/|\n|q p|   /}\n( 0 )"""\\\n|"^"`    |\n||_/=\\\\__|');
