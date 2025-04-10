118ac7af5e57c72df647eac9df50ee647981c926
NullRubia <nateriver0527@gmail.com>
2025-04-10 23:44:17 +0900

    Update 10171.js

--- level1/10171.js
+++ level1/10171.js
@@ -1,4 +1,7 @@
 console.log("\\    /\\");
-console.log(" )  ( ')");
+console.log(" )  ( ')"); //=console.log(' )  ( \')');
 console.log("(  /  )");
 console.log(" \\(__)|");
+
+//아래 방식으로도 가능( \n= 줄바꿈 문자)
+//console.log("\\    /\\\n )  ( ')\n(  /  )\n \\(__)|");
